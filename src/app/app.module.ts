17abbeef6c7de4776b9c75b5241d2d459609c2be
Chris S Kim <kisunji92@gmail.com>
2018-09-15 12:37:23 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { SearchBoxComponent } from './search/search-box/search-box.component';
+import { SearchListComponent } from './search/search-list/search-list.component';
+import { SearchItemComponent } from './search/search-list/search-item/search-item.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { FavouritesListComponent } from './favourites/favourites-list/favourites-list.component';
+import { FavouritesItemComponent } from './favourites/favourites-list/favourites-item/favourites-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SearchBoxComponent,
+    SearchListComponent,
+    SearchItemComponent,
+    FavouritesComponent,
+    FavouritesListComponent,
+    FavouritesItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/favourites/favourites-list/favourites-item/favourites-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favourites-item',
+  templateUrl: './favourites-item.component.html',
+  styleUrls: ['./favourites-item.component.css']
+})
+export class FavouritesItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
