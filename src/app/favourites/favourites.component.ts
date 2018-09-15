7dddc6d0483dd539f25364327dd188d0f07983ba
Chris S Kim <kisunji92@gmail.com>
2018-09-15 14:32:22 -0400

    Added basic bootstrap styling

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favourites.component.css']
 })
 export class FavouritesComponent implements OnInit {
+  tempList = [
+    {name: 'test1', language: 'TypeScript', latestTag: 'v0.0.1'},
+    {name: 'test2', language: 'Go', latestTag: 'v0.0.1'},
+    {name: 'test3', language: 'C#', latestTag: 'v0.0.1'},
+    {name: 'test4', language: 'Ruby', latestTag: 'v0.0.1'},
+    {name: 'test5', language: 'Python', latestTag: 'v0.0.1'}
+  ];
 
   constructor() { }
 
--- src/app/search/search-list/search-list.component.ts
+++ src/app/search/search-list/search-list.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-list.component.css']
 })
 export class SearchListComponent implements OnInit {
+  tempList = [
+    {name: 'test1', language: 'Java', latestTag: 'v0.0.1'},
+    {name: 'test2', language: 'Java', latestTag: 'v0.0.1'},
+    {name: 'test3', language: 'Java', latestTag: 'v0.0.1'},
+    {name: 'test4', language: 'Java', latestTag: 'v0.0.1'},
+    {name: 'test5', language: 'Java', latestTag: 'v0.0.1'}
+  ];
 
   constructor() { }
 
